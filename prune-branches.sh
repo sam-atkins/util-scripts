e99753e4cb268713704614d36190915a5facb46a
Sam Atkins <samatkins@outlook.com>
2018-11-23 19:59:00 +0000

    refactor prune branches fn 🍷

--- prune-branches.sh
+++ prune-branches.sh
@@ -2,20 +2,13 @@
 
 # removes local branches that are no longer on remote
 
-# soft delete if arg is -d
-prune_soft_delete() {
-  git fetch -p && for branch in `git branch -vv --no-color | grep ': gone]' | awk '{print $1}'`; do git branch -d $branch; done
+prune_branches() {
+  git fetch -p && for branch in `git branch -vv --no-color | grep ': gone]' | awk '{print $1}'`; do git branch $deleteArg $branch; done
 }
 
-# hard delete if arg is -D
-prune_hard_delete() {
-  git fetch -p && for branch in `git branch -vv --no-color | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done
-}
-
-if [ $1 = "-d" ]; then
-  prune_soft_delete
-elif [ $1 = "-D" ]; then
-  prune_hard_delete
+if [ $1 = "-d" ] || [ $1 = "-D" ]; then
+  deleteArg=$1
+  prune_branches $deleteArg
 else
   echo "Command not recognised. Try -d for soft delete; -D for hard delete";
 fi
