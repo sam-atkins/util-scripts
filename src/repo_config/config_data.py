f3375d378a2eaf6e925f6b08b8b5e1cec377a4f4
Sam Atkins <samatkins@outlook.com>
2019-09-13 16:27:53 +0100

    refactor to config based flow

---
+++ src/repo_config/config_data.py
@@ -0,0 +1,99 @@
+"""
+To add a new configuration workflow, insert a new dictionary in the CONFIG List. Each
+key/value pair is mandatory.
+
+    {
+        "config_name": "the_name_of_the_config",
+        "description": "Human readable description",
+        "language": ["python", "py"],  # add languages along with accepted abbreviations
+        "input_str": "input to confirm if the user wants the config (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],  # validation of the user's input
+        "config_format_JSON": True | False, # Bool, if true add template below.
+                                            # If False, absolute path to template
+        "file_config_template": {} | "/path/to/config_template.txt"
+        },
+        "config_destination_path": "/destination/path/filename.txt",
+        "output_message": "Optional message to print after file created",
+    }
+"""
+import os
+
+HOME = os.environ["HOME"]
+VSCODE_SETTINGS_FILE_PATH = "./.vscode/settings.json"
+
+CONFIG = [
+    {
+        "config_name": "py_formatter",
+        "description": "",
+        "language": ["python", "py"],
+        "input_str": "black is the default. Replace with yapf formatting? (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": True,
+        "file_config_template": {
+            "python.formatting.provider": "yapf",
+            "[python]": {"editor.formatOnSave": False, "editor.formatOnPaste": True},
+        },
+        "config_destination_path": VSCODE_SETTINGS_FILE_PATH,
+        "output_message": "",
+    },
+    {
+        "config_name": "pyright",
+        "description": "",
+        "language": ["python", "py"],
+        "input_str": "add pyright config (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": True,
+        "file_config_template": {
+            "include": ["src", "test"],
+            "venvPath": f"{os.getcwd()}",
+            "venv": "venv",
+            "pythonVersion": "3.7",
+        },
+        "config_destination_path": "./pyrightconfig.json",
+        "output_message": "Be sure to update the settings specific to the repo",
+    },
+    {
+        "config_name": "editorconfig",
+        "description": "",
+        "language": ["python", "py", "javascript", "js"],
+        "input_str": "add .editorconfig (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": False,
+        "file_config_template": f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/editorconfig.template",  # noqa E501
+        "config_destination_path": ".editorconfig",
+        "output_message": "Be sure to update the settings specific to the repo",
+    },
+    {
+        "config_name": "vsc_python_dev_requirements",
+        "description": "A requirements[-dev].txt of Python requirements to enhance productivity in VS Code",
+        "language": ["python", "py"],
+        "input_str": "add Python deps for VS Code (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": False,
+        "file_config_template": f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/requirements-dev.txt.template",  # noqa E501
+        "config_destination_path": "./.vscode/requirements-dev.txt",
+        "output_message": "",
+    },
+    {
+        "config_name": "setup_cfg",
+        "description": "Adds a Python setup.cfg file",
+        "language": ["python", "py"],
+        "input_str": "add setup.cfg? (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": False,
+        "file_config_template": f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/setup.cfg.template",  # noqa E501
+        "config_destination_path": "setup.cfg",
+        "output_message": "",
+    },
+    {
+        "config_name": "vscode_js_config",
+        "description": "Adds JS specific config to VS Code",
+        "language": ["javascript", "js"],
+        "input_str": "change theme for JS dev? (yes | no)?",
+        "input_validation": ["yes", "no", "y", "n"],
+        "config_format_JSON": True,
+        "file_config_template": {"workbench.colorTheme": "Default Dark+"},
+        "config_destination_path": VSCODE_SETTINGS_FILE_PATH,
+        "output_message": "",
+    },
+]
--- src/repo_config/repo_config.py
+++ src/repo_config/repo_config.py
@@ -10,46 +10,26 @@ Ensure you have a HOME env variable setup
 
 TODO(sam)
 - amend write function to append if json (./vscode/settings.json) already exists
-- refactor to make it configuration based to simplify changing a script workflow
+- add fn to validate each dict is valid with CONFIG
 - add flags e.g. --menu asks for options of what files to add; --pyright adds pyright
 """
 import argparse
-from collections import namedtuple
 import json
 import os
-from typing import List
+from typing import List, Optional
 
-PythonConfig = namedtuple(
-    "python_config",
-    ["formatter", "pyright", "editorconfig", "setup_cfg", "py_dev_deps"],
-)
-JSConfig = namedtuple("js_config", ["theme", "editorconfig"])
+from config_data import CONFIG
 
 
-HOME = os.environ["HOME"]
-EDITORCONFIG_PATH = ".editorconfig"
-EDITORCONFIG_TEMPLATE_PATH = f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/editorconfig.template"
 JSON_INDENT = 2
 JSON_SORT_KEYS = True
-PYRIGHT_CONFIG = {
-    "include": ["src", "test"],
-    "venvPath": f"{os.getcwd()}",
-    "venv": "venv",
-    "pythonVersion": "3.7",
-}
-PYRIGHT_SETTINGS_FILE_PATH = "./pyrightconfig.json"
-SETUP_CFG_PATH = "setup.cfg"
-SETUP_CFG_TEMPLATE_PATH = (
-    f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/setup.cfg.template"
-)
-VSCODE_JS_CONFIG = {"workbench.colorTheme": "Default Dark+"}
-VSCODE_PYTHON_CONFIG = {
-    "python.formatting.provider": "yapf",
-    "[python]": {"editor.formatOnSave": False, "editor.formatOnPaste": True},
-}
-VSCODE_PY_DEV_DEPENDENCIES_PATH = "./.vscode/requirements-dev.txt"
-VSCODE_PY_DEV_DEPENDENCIES_TEMPLATE_PATH = f"{HOME}/code/util-scripts/src/repo_config/repo_config_templates/requirements-dev.txt.template"
-VSCODE_SETTINGS_FILE_PATH = "./.vscode/settings.json"
+
+
+def main():
+    # TODO(sam) validate config dict has all required keys
+    language = get_cli_args()
+    config_list = build_config_list(language=language)
+    create_repo_files(config_list=config_list)
 
 
 def get_cli_args():
@@ -60,11 +40,72 @@ def get_cli_args():
     return language
 
 
+def build_config_list(language: str) -> List[str]:
+    """
+    Builds a list of config required by the user
+
+    Args:
+        language (str): language config entered by the user as CLI arg
+
+    Returns:
+        List[str]: List of configs required e.g. ['py_formatter', 'pyright']
+    """
+    config_list = []
+    for conf in CONFIG:
+        if language in conf.get("language"):
+
+            input_str = conf.get("input_str")
+            input_validation = conf.get("input_validation")
+            config_name = conf.get("config_name")
+
+            config_required = get_user_input(
+                input_str=input_str, input_validation=input_validation
+            )
+
+            if config_required.lower() in ["yes", "y"]:
+                config_list.append(config_name)
+
+    return config_list
+
+
+def get_user_input(input_str: Optional[str], input_validation: Optional[List[str]]):
+    while True:
+        user_input = input(input_str + "\n")
+        if user_input.lower() not in (input_validation):
+            print("Invalid entry, please try again")
+        else:
+            return user_input
+
+
+def create_repo_files(config_list: List[str]):
+    make_dir("./.vscode")
+
+    for conf in CONFIG:
+        name = conf.get("config_name")
+        if name in config_list:
+            config_destination_path = conf.get("config_destination_path")
+            file_config = conf.get("file_config_template")
+            output_message = conf.get("output_message")
+
+            if conf.get("config_format_JSON"):
+                write_json_file(
+                    output_file_path=config_destination_path,
+                    file_config=file_config,
+                    output_message=output_message,
+                )
+            else:
+                write_file(
+                    output_file_path=config_destination_path, file_template=file_config
+                )
+
+
 def make_dir(dir_name: str):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
+        print("Created empty ./.vscode folder")
 
 
+# TODO(sam) append if JSON file already exists
 def write_json_file(output_file_path: str, file_config: dict, output_message: str = ""):
     with open(output_file_path, "w") as outfile:
         json.dump(file_config, outfile, indent=JSON_INDENT, sort_keys=JSON_SORT_KEYS)
@@ -76,132 +117,6 @@ def write_file(output_file_path: str, file_template: str, output_message: str = 
         file_in = open(file_template)
         output_file.write(file_in.read())
     print(f"👍  File {output_file_path} created. {output_message}")
-
-
-def get_user_input(input_str: str, input_validation: List[str]):
-    while True:
-        user_input = input(input_str + "\n")
-        if user_input.lower() not in (input_validation):
-            print("Invalid entry, please try again")
-        else:
-            return user_input
-
-
-def confirm_js_config():
-    input_validation_yes_no = ["yes", "no", "y", "n"]
-    editorconfig = False
-    theme = True
-    editorconfig_required = get_user_input(
-        input_str="add .editorconfig (yes | y | no | n)?",
-        input_validation=input_validation_yes_no,
-    )
-    if editorconfig_required.lower() in ["yes", "y"]:
-        editorconfig = True
-
-    js_config = JSConfig(theme, editorconfig)
-
-    return js_config
-
-
-def confirm_python_config():
-    input_validation_yes_no = ["yes", "no", "y", "n"]
-    formatter_config = False
-    pyright_config = False
-    editorconfig = False
-    setup_cfg = False
-    py_dev_deps = False
-
-    formatter_required = get_user_input(
-        input_str="black or yapf formatting? (black | b | yapf | y)",
-        input_validation=["b", "y", "black", "yapf"],
-    )
-    if formatter_required.lower() in ["yapf", "y"]:
-        formatter_config = True
-
-    pyright_required = get_user_input(
-        input_str="add pyright config (yes | y | no | n)?",
-        input_validation=input_validation_yes_no,
-    )
-    if pyright_required.lower() in ["yes", "y"]:
-        pyright_config = True
-
-    editorconfig_required = get_user_input(
-        input_str="add .editorconfig (yes | y | no | n)?",
-        input_validation=input_validation_yes_no,
-    )
-    if editorconfig_required.lower() in ["yes", "y"]:
-        editorconfig = True
-
-    setup_cfg_required = get_user_input(
-        input_str="add setup.cfg (yes | y | no | n)?",
-        input_validation=input_validation_yes_no,
-    )
-    if setup_cfg_required.lower() in ["yes", "y"]:
-        setup_cfg = True
-
-    py_dev_deps_required = get_user_input(
-        input_str="add Python deps for VSCode (yes | y | no | n)?",
-        input_validation=input_validation_yes_no,
-    )
-    if py_dev_deps_required.lower() in ["yes", "y"]:
-        py_dev_deps = True
-
-    python_config = PythonConfig(
-        formatter_config, pyright_config, editorconfig, setup_cfg, py_dev_deps
-    )
-
-    return python_config
-
-
-def create_repo_files(language: str):
-    if language.lower() in ["python", "py"]:
-        python_config = confirm_python_config()
-        make_dir("./.vscode")
-        if python_config.formatter:
-            write_json_file(
-                output_file_path=VSCODE_SETTINGS_FILE_PATH,
-                file_config=VSCODE_PYTHON_CONFIG,
-            )
-        if python_config.pyright:
-            write_json_file(
-                output_file_path=PYRIGHT_SETTINGS_FILE_PATH,
-                file_config=PYRIGHT_CONFIG,
-                output_message="Be sure to update the settings specific to the repo",
-            )
-        if python_config.editorconfig:
-            write_file(
-                output_file_path=EDITORCONFIG_PATH,
-                file_template=EDITORCONFIG_TEMPLATE_PATH,
-            )
-        if python_config.setup_cfg:
-            write_file(
-                output_file_path=SETUP_CFG_PATH, file_template=SETUP_CFG_TEMPLATE_PATH
-            )
-        if python_config.py_dev_deps:
-            write_file(
-                output_file_path=VSCODE_PY_DEV_DEPENDENCIES_PATH,
-                file_template=VSCODE_PY_DEV_DEPENDENCIES_TEMPLATE_PATH,
-            )
-    elif language.lower() in ["js", "javascript"]:
-        js_config = confirm_js_config()
-        make_dir("./.vscode")
-        if js_config.theme:
-            make_dir("./.vscode")
-            write_json_file(
-                output_file_path=VSCODE_SETTINGS_FILE_PATH, file_config=VSCODE_JS_CONFIG
-            )
-        if js_config.editorconfig:
-            write_file(
-                output_file_path=EDITORCONFIG_PATH,
-                file_template=EDITORCONFIG_TEMPLATE_PATH,
-            )
-    else:
-        print(f"Config files not yet implemented for language {language}")
-
-
-def main():
-    language = get_cli_args()
-    create_repo_files(language=language)
 
 
 if __name__ == "__main__":
